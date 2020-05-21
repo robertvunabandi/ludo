6ded24ad9a03300ae38ec6bf05b8af19be00b753
robertvunabandi <rvunabandi@gmail.com>
2020-05-21 11:43:22 -0400

    fix js code for wait channel

--- app/javascript/channels/wait_channel.js
+++ app/javascript/channels/wait_channel.js
@@ -1,21 +1,44 @@
 import consumer from "./consumer"
 
+
 consumer.subscriptions.create("WaitChannel", {
+  //
+  // CONNECTION FUNCTIONALITIES
+  // First we're defining the four main methods called
+  // whenever connection-related things happen with connection
+  //
+
+  /**
+  * Called when the subscription is ready for use on the server
+  */
   connected() {
-    // Called when the subscription is ready for use on the server
-    console.log("Connected to Wait WaitChannel--")
+    console.log("Connected to Wait WaitChannel")
   },
 
+  /**
+  * Called when the subscription has been terminated by the server
+  */
   disconnected() {
-    // Called when the subscription has been terminated by the server
-    console.log("--Disconnected from WaitChannel")
+    console.log("Disconnected from WaitChannel")
   },
 
+  /**
+   * Called when the subscription is rejected by the server? Not sure
+   */
+  rejected() {
+    console.log("rejected!")
+  },
+
+  /**
+   * Called when there's incoming data on the websocket for this channel
+   */
   received(data) {
-    // Called when there's incoming data on the websocket for this channel
     console.log("Received data, see below")
-    console.log("---------")
     console.log(data)
-    console.log("---------")
   }
+
+  //
+  // APP FUNCTIONALITIES
+  // Things that will allow our app to work
+  //
 });
