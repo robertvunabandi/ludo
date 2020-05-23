d0cf4f2f4b142703a79db80c705135985e8212ee
robertvunabandi <rvunabandi@gmail.com>
2020-05-22 20:31:24 -0400

    import the socket into javascript

--- app/javascript/channels/wait_channel.js
+++ app/javascript/channels/wait_channel.js
@@ -1,44 +1,49 @@
 import consumer from "./consumer"
 
 
-consumer.subscriptions.create("WaitChannel", {
-  //
-  // CONNECTION FUNCTIONALITIES
-  // First we're defining the four main methods called
-  // whenever connection-related things happen with connection
-  //
+function socketSubscribe() {
+  consumer.subscriptions.create("WaitChannel", {
+    //
+    // CONNECTION FUNCTIONALITIES
+    // First we're defining the four main methods called
+    // whenever connection-related things happen with connection
+    //
 
-  /**
-  * Called when the subscription is ready for use on the server
-  */
-  connected() {
-    console.log("Connected to Wait WaitChannel")
-  },
+    /**
+    * Called when the subscription is ready for use on the server
+    */
+    connected() {
+      console.log("Connected to Wait WaitChannel")
+    },
 
-  /**
-  * Called when the subscription has been terminated by the server
-  */
-  disconnected() {
-    console.log("Disconnected from WaitChannel")
-  },
+    /**
+    * Called when the subscription has been terminated by the server
+    */
+    disconnected() {
+      this.perform("leave", {participant_id: App.participant_id})
+      console.log("Disconnected from WaitChannel")
+    },
 
-  /**
-   * Called when the subscription is rejected by the server? Not sure
-   */
-  rejected() {
-    console.log("rejected!")
-  },
+    /**
+     * Called when the subscription is rejected by the server? Not sure
+     */
+    rejected() {
+      console.log("rejected!")
+    },
 
-  /**
-   * Called when there's incoming data on the websocket for this channel
-   */
-  received(data) {
-    console.log("Received data, see below")
-    console.log(data)
-  }
+    /**
+     * Called when there's incoming data on the websocket for this channel
+     */
+    received(data) {
+      console.log("Received data, see below")
+      console.log(data)
+    }
 
-  //
-  // APP FUNCTIONALITIES
-  // Things that will allow our app to work
-  //
-});
+    //
+    // APP FUNCTIONALITIES
+    // Things that will allow our app to work
+    //
+  });
+}
+
+export default socketSubscribe;
--- app/javascript/packs/wait.js
+++ app/javascript/packs/wait.js
@@ -1,1 +1,13 @@
-require("channels/wait_channel")
+// require("channels/wait_channel")
+import socketSubscribe from "channels/wait_channel"
+
+const App = {
+  game_id: null,
+  participant_id: null,
+};
+
+window.addEventListener("load", main)
+
+function main() {
+  socketSubscribe()
+}
