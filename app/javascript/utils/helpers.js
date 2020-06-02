d728ad8755b45d122452359df160f92e16795c51
robertvunabandi <rvunabandi@gmail.com>
2020-06-02 13:32:56 -0400

    add helper methods to figure out game rule specific conditions

--- app/javascript/utils/helpers.js
+++ app/javascript/utils/helpers.js
@@ -9,7 +9,26 @@ const H = {
       }
       return o1[key] < o2[key] ? -1 : 1
     }
-  }
+  },
+  allowsSqaureDoubling(rules) {
+    return rules["allow_square_doubling"]
+  },
+  capturesIntoPrison(rules) {
+    return rules["capture_into_prison"]
+  },
+  allowsRollAfterSix(rules) {
+    return rules["allow_square_doubling"]
+  },
+  shouldStopAtGraduationEntrance(rules) {
+    return rules["graduation_lane_model"] === "strict"
+  },
+  strictAtGraduation(rules) {
+    return rules["graduation_lane_model"] === "strict"
+      || rules["graduation_lane_model"] === "strict-after-entry"
+  },
+  mustRollSixToGraduate(rules) {
+    return rules["roll_six_to_graduate"]
+  },
 }
 
 
