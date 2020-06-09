4d22778112a012d9e10d03fab01c6533fe2e3ac2
robertvunabandi <rvunabandi@gmail.com>
2020-06-09 11:43:18 -0400

    make header taller to incorporate more text

--- app/javascript/components/Game.jsx
+++ app/javascript/components/Game.jsx
@@ -424,8 +424,8 @@ function GameView(props) {
     pieces[s_color][s_id].selected = true
   }
 
-  const top_height = props.side_length * 0.11
-  const side_length = props.side_length * 0.89
+  const top_height = props.side_length * 0.13
+  const side_length = props.side_length * 0.87
 
   const rules_modal = props.display_rules
     ? makeRulesModal(props.rules, props.closeRules)
